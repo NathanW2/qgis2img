4c1d5a9c9b96d12bbba125909f93302e11619d80
Nathan Woodrow <madmanwoo@gmail.com>
2015-12-11 10:37:54 +1000

    Remove tilestash stuff

--- qgis2img.bat
+++ qgis2img.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+setlocal
 REM ---------------------------------------------------------------------------------------
 
 REM Script to setup the environment for building Roam with QGIS 2.x.
@@ -15,18 +16,18 @@ for %%x in (%cmdcmdline%) do if /i "%%~x"=="/c" set DOUBLECLICKED=1
 REM Change OSGeo4W_ROOT to point to your install of QGIS.
 
 SET OSGEO4W_ROOT=C:\OSGeo4W
-SET QGISNAME=qgis-dev
+SET QGISNAME=qgis
 SET QGIS=%OSGEO4W_ROOT%\apps\%QGISNAME%
 set QGIS_PREFIX_PATH=%QGIS%
 
 CALL %OSGEO4W_ROOT%\bin\o4w_env.bat
 
 : Python Setup
-set PATH=%OSGEO4W_ROOT%\bin;%QGIS%\bin;%PATH%
+SET PATH=%PATH%;%OSGEO4W_ROOT%\bin;%QGIS%\bin;
 SET PYTHONHOME=%OSGEO4W_ROOT%\apps\Python27
 set PYTHONPATH=%~dp0;%QGIS%\python;%PYTHONPATH%
-SET QGIS_DEBUG=0
+SET QGIS_DEBUG=1
 SET QGIS_LOG_FILE=C:\TEMP\qgis.log
 SET QGIS_DEBUG_FILE=C:\TEMP\qgis.log
-python qgis2img %*
+python %~dp0qgis2img %*
 pause
